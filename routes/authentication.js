8d221e42ecc56dd4a31f88656bf7383dad5d69d1
goncalofm90 <goncalofm90@gmail.com>
2020-07-31 11:28:50 +0100

    almostdone

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,38 @@
 'use strict';
 
 const { Router } = require('express');
-const authenticationRouter = Router();
+const router = new Router();
 
-module.exports = authenticationRouter;
+const passport = require('passport');
+
+
+router.get('/sign-up', (req, res, next) => {
+  res.render('authentication/sign-up');
+});
+
+router.post(
+  '/sign-up',
+  passport.authenticate('sign-up', {
+    successRedirect: '/',
+    failureRedirect: '/authentication/sign-up'
+  })
+);
+
+router.get('/sign-in', (req, res, next) => {
+  res.render('authentication/sign-in');
+});
+
+router.post(
+  '/sign-in',
+  passport.authenticate('sign-in', {
+    successRedirect: '/',
+    failureRedirect: '/authentication/sign-in'
+  })
+);
+
+router.post('/sign-out', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
